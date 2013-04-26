90b6a58589d42c9e99ab5fb089ec45ba1109dd47
Michael Grube <michael.grube@gmail.com>
2013-04-26 14:05:46 -0400

    removing unnecessary files.

--- testsim.py
+++
@@ -1,9 +0,0 @@
-import networkx
-import simutil
-import random
-
-g = networkx.navigable_small_world_graph(1000, 4, 2, 1, 1).to_undirected()
-simutil.graphinit(g)
-attackers = list()
-simutil.pickmalnodes(g, attackers, 4)
-simutil.sandbergsolution(g, attackers, .037)
